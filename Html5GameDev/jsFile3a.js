8c51e276da056ba0a3f22d8d1061193d6adb6ce8
Santiago Balladares <santiago.nib@live.com>
2011-05-25 14:39:47 +0100

    Third week Task A

--- Html5GameDev/jsFile3a.js
+++ Html5GameDev/jsFile3a.js
@@ -1,4 +1,4 @@
-//### Constants
+// Constants
 const LEFT_ARROW = 37;
 const RIGHT_ARROW = 39;
 const UP_ARROW = 38;
@@ -10,10 +10,125 @@ const LEAF_COLOR = "#1F5017";
 const TRUNK_COLOR = "#5E3F18";
 const STONE_COLOR = "#817D85";
 
-// ### Global variables
+// Global variables
 var up, down, left, right;
 
-// ### Player Object
+// A mountain background scenery object with getLeft, getRight, getTop, getBottom, and draw methods:
+function Mountain(x, y, height, width) {
+	this.x = x;
+	this.y = y;
+	this.height = height;
+	this.width = width;
+	this.left = x;
+	this.right = x + width;
+	this.top = y - height;
+	this.bottom = y;
+}
+Mountain.prototype = {
+	getLeft: function() {
+		return this.left;
+	},
+	
+	getRight: function() {
+		return this.right;
+	},
+	
+	getTop: function() {
+		return this.top;
+	},
+	
+	getBottom: function() {
+		return this.bottom;
+	},
+	
+	draw: function(ctx) {
+		ctx.fillStyle = MOUNTAINS_COLOR;
+		ctx.beginPath();
+		ctx.moveTo(this.left, this.bottom);
+		ctx.lineTo(this.left + this.width/2, this.top);
+		ctx.lineTo(this.right, this.bottom);
+		ctx.fill();
+		ctx.stroke();
+	}
+};
+
+// A tree background scenery object with getLeft, getRight, getTop, getBottom, and draw methods:
+function Tree(x, y, trunkHeight, trunkWidth, leafWidth) {
+	this.trunkHeight = trunkHeight;
+	this.trunkWidth = trunkWidth;
+	this.leafWidth = leafWidth;
+	
+	this.left = x;
+	this.right = x + this.leafWidth;
+	this.top = y - (this.trunkHeight + this.leafWidth/2);
+	this.bottom = y;
+	this.centerX = x + this.leafWidth/2;
+	this.trunkLeft = this.centerX - this.trunkWidth/2;
+	this.trunkRight = this.centerX + this.trunkWidth/2;
+}
+Tree.prototype = {
+	getLeft: function() {
+		return this.left;
+	},
+
+	getRight: function() {
+		return this.right;
+	},
+	
+	getTop: function() {
+		return this.top;
+	},
+
+	getBottom: function() {
+		return this.bottom;
+	},
+
+	draw: function(ctx) {
+		ctx.fillStyle = TRUNK_COLOR;
+		ctx.fillRect(this.trunkLeft, this.bottom, this.trunkWidth, -this.trunkHeight);
+		ctx.fillStyle = LEAF_COLOR;
+		ctx.beginPath();
+		ctx.arc(this.centerX, this.bottom - this.trunkHeight, this.leafWidth/2, 0, Math.PI * 2, false);
+		ctx.fill();
+		ctx.stroke();
+	}
+};
+
+// A lake background scenery object with getLeft, getRight, getTop, getBottom, and draw methods:
+function Lake(x, y, size) {
+	this.size = size;
+	this.left = x;
+	this.right = x + this.size;
+	this.top = y - this.size;
+	this.bottom = y;
+}
+Lake.prototype = {
+	getLeft: function() {
+		return this.left;
+	},
+
+	getRight: function() {
+		return this.right;
+	},
+	
+	getTop: function() {
+		return this.top;
+	},
+
+	getBottom: function() {
+		return this.bottom;
+	},
+	
+	draw: function(ctx) {
+		ctx.fillStyle = WATER_COLOR;
+		ctx.beginPath();
+		ctx.arc(this.left + this.size/2, this.top + this.size/2, this.size/2, 0, Math.PI * 2, false);
+		ctx.fill();
+		ctx.stroke();
+	}
+}
+
+// Player Object
 function ConstrPlayer(filename, x, y) {
 	this.x = x;
 	this.y = y;
@@ -21,8 +136,7 @@ function ConstrPlayer(filename, x, y) {
 	this.height = 64;
 	this.init(filename);
 }
-
-// ### Player Prototype
+// Player Prototype
 ConstrPlayer.prototype = {
 	imgLoaded: false,
 
@@ -44,16 +158,16 @@ ConstrPlayer.prototype = {
 	},
 	  
 	getTop: function() {
-		return this.y;
+		return this.y - this.height;
 	},
 	
-	getbottom: function() {
-		return this.y + this.height;
+	getBottom: function() {
+		return this.y;
 	},
 	  
 	draw: function(ctx) {
 		if (this.imgLoaded) {
-			ctx.drawImage(this.img, this.x, this.y - this.height);
+			ctx.drawImage(this.img, this.x, this.y);
 		}
 	},
 
@@ -66,6 +180,7 @@ ConstrPlayer.prototype = {
 	}
 };
 
+// The World
 var TheWorld = {
 	xOffset: 0,  // how far to the right has the view scrolled from its starting location
 	yOffset: 0,
@@ -73,8 +188,8 @@ var TheWorld = {
 	canvasHeight: 400,
 	leftScrollMargin: 200, // if you go left of 200 pixels the screen scrolls left
 	rightScrollMargin: 400,  // if you go right of 400 pixels the screen scrolls right
-	upScrollMargin: 100, // if you go up of 150 pixels the screen scrolls up
-	downScrollMargin: 300,  // if you go down of 150 pixels the screen scrolls down
+	topScrollMargin: 133, // if you go up of 150 pixels the screen scrolls up
+	bottomScrollMargin: 266,  // if you go down of 150 pixels the screen scrolls down
 
 	// keep a list of background objects and a list of foreground objects --
 	// all of these will get drawn 
@@ -97,10 +212,12 @@ var TheWorld = {
 		return worldY - this.yOffset;
 	},
 
-	scrollIfNeeded: function(player) { //TO CHECK LATER
-		// get screen coordinates of player's left and right edges
+	scrollIfNeeded: function(player) {
+		// get screen coordinates of player's four edges
 		var screenLeft = this.worldXToScreenX(player.getLeft());
 		var screenRight = this.worldXToScreenX(player.getRight());
+		var screenTop = this.worldYToScreenY(player.getTop());
+		var screenBottom = this.worldYToScreenY(player.getBottom());
 
 		// if player's left is left of left scroll margin, scroll left
 		if (screenLeft < this.leftScrollMargin) {
@@ -111,6 +228,16 @@ var TheWorld = {
 		if (player.getRight() - this.xOffset > this.rightScrollMargin) {
 			// scroll by just enough to get player's right lined up with right scroll margin
 			this.xOffset += screenRight - this.rightScrollMargin;
+		}
+		// if player's top is up of top scroll margin, scroll up
+		if (screenTop < this.topScrollMargin) {
+			// scroll by just enough to get player's top lined up with top scroll margin
+			this.yOffset -= this.topScrollMargin - screenTop;
+		}
+		// if player's bottom is down of bottom scroll margin, scroll down
+		if (player.getBottom() - this.yOffset > this.bottomScrollMargin) {
+			// scroll by just enough to get player's bottom lined up with bottom scroll margin
+			this.yOffset += screenBottom - this.bottomScrollMargin;
 		}
 	},
 
@@ -123,12 +250,12 @@ var TheWorld = {
 		if (this.worldXToScreenX(obj.getLeft()) > this.canvasWidth) {
 			return;
 		}
-		// if (this.worldYToScreenY(obj.getDown()) < 0) {
-			// return;
-		// }
-		// if (this.worldYToScreenY(obj.getTop()) > this.canvasHeight) {
-			// return;
-		// }
+		if (this.worldYToScreenY(obj.getBottom()) < 0) {
+			return;
+		}
+		if (this.worldYToScreenY(obj.getTop()) > this.canvasHeight) {
+			return;
+		}
 		obj.draw(ctx);
 	},
   
@@ -139,7 +266,7 @@ var TheWorld = {
 
 		// Now apply the translate transform to scroll the world
 		ctx.save();
-		ctx.translate(0 - this.xOffset, this.canvasHeight); 
+		ctx.translate(0 - this.xOffset, 0 - this.yOffset); 
 		
 		// draw all background objects in their scrolled location
 		var i, obj;
@@ -155,80 +282,37 @@ var TheWorld = {
 	}
 };
 
-// A random background scenery object with getLeft, getRight, and draw methods:
-function Mountain(x, size) {
-  this.left = x;
-  this.size = size;
-  this.right = this.left + this.size * 2;
-}
-Mountain.prototype = {
-  getLeft: function() {
-    return this.left;
-  },
-
-  getRight: function() {
-    return this.right;
-  },
-
-  draw: function(ctx) {
-    ctx.fillStyle = GROUND_COLOR;
-    ctx.beginPath();
-    ctx.moveTo(this.left, 0);
-    ctx.lineTo(this.left + this.size, 0 - this.size);
-    ctx.lineTo(this.right, 0);
-    ctx.fill();
-    ctx.stroke();
-  }
-};
-
-// A random background scenery object with getLeft, getRight, and draw methods:
-function Tree(x, trunkHeight, leafWidth) {
-  this.left = x;
-  this.leafWidth = leafWidth;
-  this.right = this.left + this.leafWidth * 2;
-  this.center = this.left + this.leafWidth;
-  this.trunkLeft = this.center - 3;
-  this.trunkRight = this.center + 3;
-  this.trunkHeight = trunkHeight;
-  this.trunkWidth = 20;
-}
-Tree.prototype = {
-  getLeft: function() {
-    return this.left;
-  },
-
-  getRight: function() {
-    return this.right;
-  },
-
-  draw: function(ctx) {
-    ctx.fillStyle = TRUNK_COLOR;
-    ctx.fillRect(this.trunkLeft, 0 - this.trunkHeight, this.trunkWidth, this.trunkHeight);
-    ctx.fillStyle = LEAF_COLOR;
-    ctx.beginPath();
-    ctx.arc(this.center, 0 - this.trunkHeight, this.leafWidth, 0, Math.PI * 2, false);
-    ctx.fill();
-    ctx.stroke();
-  }
-};
-
 $(document).ready(function() {
 	var context = $("#game_canvas")[0].getContext("2d");
+	var player = new ConstrPlayer("images/LinkFront.gif", 200, -200);
 
-	var player = new ConstrPlayer("images/LinkFront.gif", 200, 0);
+	// Make some background objects:
+	TheWorld.addBackgroundObject(new Mountain(0, -550, 100, 120));
+	TheWorld.addBackgroundObject(new Mountain(-30, -550, 90, 100));
+	TheWorld.addBackgroundObject(new Mountain(20, -550, 120, 150));
+	TheWorld.addBackgroundObject(new Mountain(-150, -400, 80, 100));
+	TheWorld.addBackgroundObject(new Mountain(-100, -400, 100, 100));
+	TheWorld.addBackgroundObject(new Mountain(100, -350, 80, 100));
+	TheWorld.addBackgroundObject(new Mountain(150, -350, 100, 100));
+	TheWorld.addBackgroundObject(new Mountain(300, -500, 80, 100));
+	TheWorld.addBackgroundObject(new Mountain(350, -500, 100, 100));
+	
+	TheWorld.addBackgroundObject(new Mountain(900, 0, 80, 100));
+	TheWorld.addBackgroundObject(new Mountain(950, 0, 100, 100));
+	TheWorld.addBackgroundObject(new Mountain(700, 150, 80, 100));
+	TheWorld.addBackgroundObject(new Mountain(750, 150, 100, 100));
+
+	TheWorld.addBackgroundObject(new Tree(-40, 10, 70, 15, 50));
+	TheWorld.addBackgroundObject(new Tree(10, -30, 70, 15, 55));
+	TheWorld.addBackgroundObject(new Tree(100, 0, 70, 12, 45));
+	TheWorld.addBackgroundObject(new Tree(140, 40, 70, 10, 40));
+	TheWorld.addBackgroundObject(new Tree(200, -50, 70, 17, 60));
+	
+	TheWorld.addBackgroundObject(new Lake(280, 300, 100));
 
 	// Put the player in the world:
 	TheWorld.addForegroundObject(player);
-
-  // Make some background objects:
-  TheWorld.addBackgroundObject(new Tree(30, 60, 30));
-  TheWorld.addBackgroundObject(new Tree(180, 80, 30));
-  TheWorld.addBackgroundObject(new Tree(260, 60, 30));
-  TheWorld.addBackgroundObject(new Mountain(400, 150));
-  TheWorld.addBackgroundObject(new Mountain(600, 200));
-  TheWorld.addBackgroundObject(new Tree(1200, 80, 40));
-  TheWorld.addBackgroundObject(new Tree(1300, 40, 20));
-
+	
 	TheWorld.draw(context);
 
 	// Keydown sets Timer
